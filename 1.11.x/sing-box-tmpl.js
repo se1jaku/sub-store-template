878e682e16e34b5e3b2b187d29e0091e71b382cf
seijaku <seijaku@mail.com>
2025-06-13 04:00:04 +0800

    update

--- 1.11.x/sing-box-tmpl.js
+++ 1.11.x/sing-box-tmpl.js
@@ -29,16 +29,38 @@ config.outbounds.map(i => {
     i.outbounds.push(...getTags(proxies, /^🇼🇸\s+(台湾|tw)-(hinet)/i))
   }
   if (['🇸🇬 新加坡', '🇸🇬 新加坡【自动】'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /^🇸🇬\s+(新加坡|sg)-(rfc|jinx|ctc|dmit|bage|enos)/i))
-  }
-  if (['🇸🇬 新加坡原生', '🇸🇬 新加坡原生【自动】'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /^🇸🇬\s+(新加坡|sg)-(halo)/i))
+    i.outbounds.push(...getTags(proxies, /^🇸🇬\s+(新加坡|sg)-(rfc|dmit|bage|enos)/i))
   }
   if (['🇯🇵 日本', '🇯🇵 日本【自动】'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /^🇯🇵\s+(日本|jp)-(rfc|jinx|ctc|dmit|bage|enos)/i))
+    i.outbounds.push(...getTags(proxies, /^🇯🇵\s+(日本|jp)-(rfc|dmit|bage|enos)/i))
   }
   if (['🇺🇸 美国', '🇺🇸 美国【自动】'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /^🇺🇸\s+(美国|us)-(rfc|jinx|ctc|dmit|bage|enos)/i))
+    i.outbounds.push(...getTags(proxies, /^🇺🇸\s+(美国|us)-(rfc|dmit|bage|enos|misaka)/i))
+  }
+  if (['🇩🇪 德国', '🇩🇪 德国【自动】'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /^🇺🇸\s+(德国|us)-(rfc|dmit|bage|enos|misaka)/i))
+  }
+  // original
+  if (['🇸🇬 新加坡原生', '🇸🇬 新加坡原生【自动】'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /^🇸🇬\s+(新加坡|sg)-(orig|halo)/i))
+  }
+  if (['🇲🇾 马来西亚原生', '🇲🇾 马来西亚原生【自动】'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /^🇲🇾\s+(马来西亚|my)-(orig|terabix)/i))
+  }
+  if (['🇦🇺 澳大利亚原生', '🇦🇺 澳大利亚原生【自动】'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /^🇦🇺\s+(澳大利亚|au)-(orig|wepc)/i))
+  }
+  if (['🇺🇸 美国原生', '🇺🇸 美国原生【自动】'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /^🇺🇸\s+(美国|us)-([a-z]+-)?(orig|bage)/i))
+  }
+  if (['🇩🇪 德国原生', '🇩🇪 德国原生【自动】'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /^🇩🇪\s+(德国|de)-(orig|bage)/i))
+  }
+  if (['🇬🇧 英国原生', '🇬🇧 英国原生【自动】'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /^🇬🇧\s+(英国|uk)-(orig|bage)/i))
+  }
+  if (['🇳🇱 荷兰DCMA', '🇳🇱 荷兰DCMA【自动】'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /^🇳🇱\s+(荷兰|nl)-(orig|dcma)/i))
   }
 })
 
